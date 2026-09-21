b0c84203e28d71f5b362d5cb7c031561ec1fbbc8
Pronoy-2003 <pronoypryunkushsonowal2003@gmail.com>
2026-09-22 00:03:01 +0530

    Update create_tables.sql

--- sql/03_bronze/create_tables.sql
+++ sql/03_bronze/create_tables.sql
@@ -7,32 +7,35 @@
 */
 
 
--- Create bronze.stock_price_raw
-CREATE TABLE bronze.stock_price_raw
-(
-    bronze_id BIGINT IDENTITY(1,1) NOT NULL,
-
-    ingestion_id UNIQUEIDENTIFIER NOT NULL,
-
-    symbol VARCHAR(10) NOT NULL,
-
-    observation_date DATE NOT NULL,
-
-    open_price DECIMAL(18,4) NULL,
-    high_price DECIMAL(18,4) NULL,
-    low_price DECIMAL(18,4) NULL,
-    close_price DECIMAL(18,4) NULL,
-
-    volume BIGINT NULL,
-
-    source VARCHAR(50) NOT NULL,
-
-    loaded_at DATETIME2 NOT NULL
-        DEFAULT SYSDATETIME(),
-
-    CONSTRAINT PK_bronze_stock_price_raw
-        PRIMARY KEY (bronze_id)
-);
+-- Create bronze.stock_price_raw table
+IF OBJECT_ID('bronze.stock_price_raw', 'U') IS NULL
+BEGIN
+    CREATE TABLE bronze.stock_price_raw
+    (
+        bronze_id BIGINT IDENTITY(1,1) NOT NULL,
+    
+        ingestion_id UNIQUEIDENTIFIER NOT NULL,
+    
+        symbol VARCHAR(10) NOT NULL,
+    
+        observation_date DATE NOT NULL,
+    
+        open_price DECIMAL(18,4) NULL,
+        high_price DECIMAL(18,4) NULL,
+        low_price DECIMAL(18,4) NULL,
+        close_price DECIMAL(18,4) NULL,
+    
+        volume BIGINT NULL,
+    
+        source VARCHAR(50) NOT NULL,
+    
+        loaded_at DATETIME2 NOT NULL
+            DEFAULT SYSDATETIME(),
+    
+        CONSTRAINT PK_bronze_stock_price_raw
+            PRIMARY KEY (bronze_id)
+    );
+END;
 GO
 
 -- Add a unique constraint
