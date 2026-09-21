41b2d176cff2509035e9c3bd01d03ab000058050
Pronoy-2003 <pronoypryunkushsonowal2003@gmail.com>
2026-09-22 00:03:58 +0530

    Update ingestion_log.sql

--- sql/03_bronze/ingestion_log.sql
+++ sql/03_bronze/ingestion_log.sql
@@ -8,23 +8,26 @@
 
 
 -- Create bronze.ingestion_log
-CREATE TABLE bronze.ingestion_log
-(
-    ingestion_id UNIQUEIDENTIFIER NOT NULL,
-
-    pipeline_name VARCHAR(100) NOT NULL,
-
-    start_time DATETIME2 NOT NULL,
-
-    end_time DATETIME2 NULL,
-
-    status VARCHAR(20) NOT NULL,
-
-    records_received INT DEFAULT 0,
-
-    error_message VARCHAR(2000) NULL,
-
-    CONSTRAINT PK_bronze_ingestion_log
-        PRIMARY KEY (ingestion_id)
-);
+IF OBJECT_ID('bronze.ingestion_log', 'U') IS NULL
+BEGIN
+    CREATE TABLE bronze.ingestion_log
+    (
+        ingestion_id UNIQUEIDENTIFIER NOT NULL,
+    
+        pipeline_name VARCHAR(100) NOT NULL,
+    
+        start_time DATETIME2 NOT NULL,
+    
+        end_time DATETIME2 NULL,
+    
+        status VARCHAR(20) NOT NULL,
+    
+        records_received INT DEFAULT 0,
+    
+        error_message VARCHAR(2000) NULL,
+    
+        CONSTRAINT PK_bronze_ingestion_log
+            PRIMARY KEY (ingestion_id)
+    );
+END;
 GO
